5ed44ee3a5e5361af9989250b0d63da6f4a26956
bwritam <bwritam@gmail.com>
2021-08-09 11:19:08 +0530

    commit2

--- main.js
+++ main.js
@@ -2,25 +2,29 @@ document.querySelector(".btn").addEventListener("click", displayError);
 
 
 function displayError() {
-	var fname = document.querySelector("#fname").value;
-	var lname = document.querySelector("#lname").value;
-	var pwd = document.querySelector("#pwd").value;
-	var email = document.querySelector("#email").value;
+	var fname = document.querySelector(".fname").value;
+	var lname = document.querySelector(".lname").value;
+	var pwd = document.querySelector(".pwd").value;
+	var email = document.querySelector(".email").value;
 	// var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 	if (fname === "") {
 		document.querySelector(".text-one").innerHTML = "First Name cannot be empty";
+		document.querySelector(".fname").className = "error-image";
 	}
 
 	if (lname === "") {
 		document.querySelector(".text-two").innerHTML = "Last Name cannot be empty";
+		document.querySelector(".lname").className = "error-image";
 	}
 
 	if (email === "") {
 		document.querySelector(".text-three").innerHTML = "Looks like this is not an email";
+		document.querySelector(".email").className = "error-image";
 	}
 
 	if (pwd === "") {
 		document.querySelector(".text-four").innerHTML = "Password cannot be empty";
+		document.querySelector(".pwd").className = "error-image";
 	}
 }
